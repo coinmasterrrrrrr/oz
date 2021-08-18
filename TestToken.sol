7fa71266ec0c54166283bd378ba24ca379e2fccd
coinmasterrrrrrr <89160123+coinmasterrrrrrr@users.noreply.github.com>
2021-08-18 12:40:41 -0700

    Create TestToken.sol

---
+++ TestToken.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "./OwnableToken.sol";
+
+
+contract TestToken is OwnableToken {
+  constructor() OwnableToken("Test Token", "TST") {}
+}
